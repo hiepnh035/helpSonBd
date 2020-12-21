3fc2d74487fc5d720669e6a5e7278deec7af4682
HIEPNG <hiep.nguyenhuy@ifisolution.com>
2020-12-21 11:01:13 +0100

    fix page transition

--- assignment_cho_SonBd/script.js
+++ assignment_cho_SonBd/script.js
@@ -8,13 +8,33 @@ function startAttempt() {
     attemptQuiz.style.display = "block";
     introduction.style.display = "none";
     reviewQuiz.style.display = "none";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://wpr-quiz-api.herokuapp.com/attempts", true);
+    xhttp.send();
+    console.log(xhttp.response);
 }
 
 function toggleReview() {
+    var quizName = document.getElementById("quiz-name");
     var introduction = document.getElementById("introduction");
     var attemptQuiz = document.getElementById("attempt-quiz");
     var reviewQuiz = document.getElementById("review-quiz");
     attemptQuiz.style.display = "none";
     introduction.style.display = "none";
     reviewQuiz.style.display = "block";
+    var form  = document.getElementById("question-form");
+    form.onsubmit = function(e) {
+        e.preventDefault();
+        quizName.scrollIntoView();
+    }
+}
+
+function tryAgain() {
+    var introduction = document.getElementById("introduction");
+    var attemptQuiz = document.getElementById("attempt-quiz");
+    var reviewQuiz = document.getElementById("review-quiz");
+    introduction.style.display = "block";
+    reviewQuiz.style.display = "none";
+    attemptQuiz.style.display = "none";
 }
