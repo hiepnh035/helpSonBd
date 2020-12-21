7b6c101b2944c15b3e17db3fcb42bf55eaa5a304
HIEPNG <hiep.nguyenhuy@ifisolution.com>
2020-12-21 09:59:00 +0100

    add question and review

--- assignment_cho_SonBd/script.js
+++ assignment_cho_SonBd/script.js
@@ -1,1 +1,20 @@
 // TODO(you): Write the JavaScript necessary to complete the assignment.
+function handleCheck() {}
+
+function startAttempt() {
+    var introduction = document.getElementById("introduction");
+    var attemptQuiz = document.getElementById("attempt-quiz");
+    var reviewQuiz = document.getElementById("review-quiz");
+    attemptQuiz.style.display = "block";
+    introduction.style.display = "none";
+    reviewQuiz.style.display = "none";
+}
+
+function toggleReview() {
+    var introduction = document.getElementById("introduction");
+    var attemptQuiz = document.getElementById("attempt-quiz");
+    var reviewQuiz = document.getElementById("review-quiz");
+    attemptQuiz.style.display = "none";
+    introduction.style.display = "none";
+    reviewQuiz.style.display = "block";
+}
